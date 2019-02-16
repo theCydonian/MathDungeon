a70e3e3b2be677d22fe89616c0356f956ddbf529
SuperHeckMan <macbookair13@Big-Man-MacBook-Pro.local>
2019-02-16 13:32:48 -0800

    Line aiming system works

--- Unity/Assets/Scripts/PlayerControl.cs
+++ Unity/Assets/Scripts/PlayerControl.cs
@@ -4,23 +4,51 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // A marker for where the mouse is
-    private GameObject aimObject;
+    public GameObject pointObject;
+    // y - y1 = m(x - x1)
+    [Range(-10, 10)]
+    public float mSlope = 2.0f;
 
+    private Vector2 playerPosition;
+    private Vector3 Y_function;
+
+    private bool changeParams = false;
+    public int range;
+    GameObject[] pointGuide;
     // Start is called before the first frame update
     void Start()
     {
-        
+       pointGuide = new GameObject[range];
+
+        for (int i = 0; i < range ; i++)
+        {
+            pointGuide[i] = (GameObject)Instantiate(pointObject);
+            pointGuide[i].transform.parent = null;
+
+        }
     }
 
-    // Update is called once per frame
+    // 
     void Update()
     {
-        Look();
+        for (int i = 0; i < range; i++)
+        {
+            float d_x = (i + 0.5f) / 5f;
+
+            playerPosition = new Vector2(transform.localPosition.z, transform.localPosition.x);
+
+            float function = mSlope * d_x;
+
+            Y_function.z = d_x - playerPosition.y;
+            Y_function.x = -function - playerPosition.x;
+
+            pointGuide[i].transform.localPosition = Y_function;
+            
+        }
     }
 
-    void Look()
+    void Aiming()
     {
-        
+
     }
 }
