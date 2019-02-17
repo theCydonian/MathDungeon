1b34d988365706b54796ffd559677b28c8664baa
theCydonian <31706764+theCydonian@users.noreply.github.com>
2019-02-16 16:32:20 -0800

    parsing works | collision detect doesnt

---
+++ Unity/Assets/Scripts/MoveProjectiles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveProjectiles : MonoBehaviour {
+	// Use this for initialization
+	private FunctionControl fx;
+	public int dir; // 1 = right; -1 = left;
+	public double amount;
+	void Start () {
+		fx = GameObject.FindGameObjectWithTag ("Player").GetComponent<FunctionControl> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3((float)fx.output(dir*amount + transform.position.z), transform.position.y, (float)(transform.position.z + dir * amount));
+	}
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.tag == "Wall") {
+			print ("hello");
+		}
+	}
+}
